ed8297280f75e4f501b45cd54e783632598354ff
Eylül Kırdak <72758485+eylulkirdak@users.noreply.github.com>
2021-12-28 18:18:49 +0300

    list items tab

--- config.php
+++ config.php
@@ -4,4 +4,4 @@
     if ($db->connect_errno > 0){
         die('Unable to connect to database [' . $db->connect_errno . ']');
     }
-?>
+?>  
--- getitems.php
+++ getitems.php
@@ -1,14 +1,24 @@
 <?php
 
     include "config.php";
-?>
 
-<html>
+    if (isset($_POST['bname']))
+    {   
+        $bname = $_POST['bname'];
 
-    <form>
-        <select>
+        $sql_checker = "SELECT * FROM BOOKS WHERE bname='$bname'";
+        $result_checker = mysqli_query($db, $sql_checker);
+        if (mysqli_num_rows($result_checker)==0){
+            echo '<script>alert("The book does not exist in our database, please make sure that the book name is correct")</script>';
+        }
+        else {
+            echo '<script>alert("Yes it exists!")</script>';
             
-        </select>
-    </form>
-
-</html>
+        }
+        
+        if (false===$result_checker) {
+            printf("error: %s\n", mysqli_error($db));
+        }
+    }
+    header("Location: getitems.html");
+?>
