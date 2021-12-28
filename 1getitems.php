9cd437d250f3b614ed47ed3965457613a97b49f9
Mustafa Ahmet Yücel <74565914+mustafaaycll@users.noreply.github.com>
2021-12-28 18:34:55 +0300

    fixed new page on non existence

---
+++ 1getitems.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <?php
+
+    include "config.php";
+
+    if (isset($_POST['bname'])) {
+        $bname = $_POST['bname'];
+
+        $sql_checker = "SELECT * FROM BOOKS WHERE bname='$bname'";
+        $result_checker = mysqli_query($db, $sql_checker);
+        if (mysqli_num_rows($result_checker) == 0) {
+            echo '<script>alert("The book does not exist in our database, please make sure that the book name is correct")</script>';
+        } else {
+            echo '<script>alert("Yes it exists!")</script>';
+        }
+
+        if (false === $result_checker) {
+            printf("error: %s\n", mysqli_error($db));
+        }
+    }
+    header("Location: getitems.html");
+    ?>
+
+</html>
+</body>
--- getitems.php
+++ getitems.php
@@ -1,24 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-    include "config.php";
+<head>
+    <title>libman - Library Management System</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
 
-    if (isset($_POST['bname']))
-    {   
-        $bname = $_POST['bname'];
+<body>
+    <header>
+        <h1>libman</h1>
+        <nav>
+            <ul class="nav__links">
+                <li><a href="insertrecord.html">Add Borrow Record</a></li>
+                <li><a href="removerecord.html">Remove Borrow Record</a></li>
+                <li><a href="insertbook.html">Add Book</a></li>
+                <li><a href="removebook.html">Remove Book</a></li>
+                <li><a href="insertuser.html">Add/Remove User</a></li>
+                <li><a href="getitems.html">List Items</a></li>
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <br>
+    <br>
+    <div align="center">
+        <br>
+        <form method="POST">
+            <h1>Please type in the name of the book you'd like to search for.<br><br>
+                <input type="text" name="bname" placeholder="Book Name"><br><br>
+                <button>SEARCH</button>
+        </form>
 
-        $sql_checker = "SELECT * FROM BOOKS WHERE bname='$bname'";
-        $result_checker = mysqli_query($db, $sql_checker);
-        if (mysqli_num_rows($result_checker)==0){
-            echo '<script>alert("The book does not exist in our database, please make sure that the book name is correct")</script>';
-        }
-        else {
-            echo '<script>alert("Yes it exists!")</script>';
-            
-        }
+        <?php
+
+        include "config.php";
         
-        if (false===$result_checker) {
-            printf("error: %s\n", mysqli_error($db));
+        if (isset($_REQUEST['bname'])) {
+            $bname = $_REQUEST['bname'];
+
+            $sql_checker = "SELECT * FROM BOOKS WHERE bname='$bname'";
+            $result_checker = mysqli_query($db, $sql_checker);
+            if (mysqli_num_rows($result_checker) == 0) {
+                echo '<script>alert("The book does not exist in our database, please make sure that the book name is correct")</script>';
+            } else {
+                header('Location: index.html');
+            }
         }
-    }
-    header("Location: getitems.html");
-?>
+        ?>
+    </div>
+</body>
+
+</html>
